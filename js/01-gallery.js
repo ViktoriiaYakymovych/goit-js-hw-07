d55154d599043ee9e818d9144ec4828b52216ae5
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-04-27 11:59:17 +0300

    solved task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const listItems = makeMarkup(galleryItems);
+fullList(listItems);
+
+galleryList.addEventListener("click", onImgClick);
+
+function makeMarkup(elements) {
+    return elements.map((el) => {
+        return `<li class="gallery__item">
+                <a href="${el.original}" class="gallery__link">
+                    <img class="gallery__image" src="${el.preview}" alt="${el.description}" data-source="${el.original}">
+                </a>
+                </li>`;
+    }).join("");
+}
+
+function fullList(markup) {
+    galleryList.innerHTML = markup;
+}
+
+function onImgClick(e) {
+    e.preventDefault();
+    const img = e.target;
+
+    if (img.nodeName !== "IMG") {
+        return;
+    }
+
+    const basicImg = basicLightbox.create(`    <img src="${img.dataset.source}" width="600" height="400">`);
+
+    basicImg.show();
+    window.addEventListener("keydown", onEscapeClick);
+    function onEscapeClick(e) {
+        if (e.code === "Escape") {
+            basicImg.close();
+            window.removeEventListener("keydown", onEscapeClick);
+        }
+    }
+}
+
+// console.log(galleryItems);
+// console.log(galleryList);
